2e398b550d2ff3d6ad705fdaa755336f46492a9e
urielatomic <uriel@eatatomic.com>
2026-04-10 10:09:17 -0300

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,13 +1,10 @@
+import React from "react"
+
 export const metadata = {
   title: "Atomic Rush",
-  description: "Juego de reacción",
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
       <body style={{ margin: 0, padding: 0 }}>
