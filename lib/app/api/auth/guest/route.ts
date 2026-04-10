a2f84e711fd1827ecbfc89f3c0e02bcbf19beee0
urielatomic <uriel@eatatomic.com>
2026-04-10 11:30:15 -0300

    Update route.ts

--- lib/app/api/auth/guest/route.ts
+++ lib/app/api/auth/guest/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { supabaseAdmin } from '../../../../lib/supabase'
+import { supabaseAdmin } from '@/lib/supabase'
 
 export async function POST() {
   const { data: user, error } = await supabaseAdmin
